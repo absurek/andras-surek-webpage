b3ec1a6868cb3bad6ba1f38338ce5de55e8f77cf
absurek <surek.andras@gmail.com>
2023-09-23 20:35:45 +0200

    Navbar skeleton

---
+++ src/config/navigation.ts
@@ -0,0 +1,11 @@
+export type NavigationLink = {
+  label: string;
+  url: string;
+};
+
+export const navigationLinks: readonly NavigationLink[] = [
+  { label: 'Főoldal', url: '/' },
+  { label: 'Bemutatkozás', url: '/' },
+  { label: 'Blog', url: '/' },
+  { label: 'Portfólió', url: '/' },
+];
