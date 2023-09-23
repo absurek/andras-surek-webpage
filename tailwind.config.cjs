7e44d42467b1b5c9663044f927b4846a1b58cfd4
absurek <surek.andras@gmail.com>
2023-09-23 02:00:11 +0200

    Navbar added to Layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,7 @@ const { fontFamily } = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	darkMode: ['system'],
+	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
     container: {
