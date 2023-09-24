0efdc89dad6d71ce6cb26140068851f433e21099
absurek <surek.andras@gmail.com>
2023-09-24 19:29:34 +0200

    Astro config updated with site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import { site } from '@/config/site';
+
 // https://astro.build/config
 export default defineConfig({
+  site,
   integrations: [tailwind()],
 });
