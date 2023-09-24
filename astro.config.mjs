1abb563459807075f23fc2b8be83e1112a44200c
absurek <surek.andras@gmail.com>
2023-09-24 19:57:39 +0200

    Config import fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-import { site } from '@/config/site';
+import { site } from './src/config/site';
 
 // https://astro.build/config
 export default defineConfig({
